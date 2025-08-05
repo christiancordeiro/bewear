c917dc6bd98bc4ddec75f43e7e69446c573a8881
christiancordeiro <christiancordeiro96@gmail.com>
2025-08-05 17:17:44 -0300

    feat: add ui for sign in and sign up form components

---
+++ src/app/authentication/components/sign-up.form.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z
+  .object({
+    nome: z.string("Nome é obrigatório").trim().min(1, "Nome é obrigatório"),
+    email: z.email("Email inválido"),
+    senha: z.string().min(8, "Senha inválida"),
+    confirmacaoSenha: z.string().min(8, "Senha inválida"),
+  })
+  .refine((data) => data.senha === data.confirmacaoSenha, {
+    error: "As senhas não coincidem",
+    path: ["confirmacaoSenha"],
+  });
+
+type FormValues = z.infer<typeof formSchema>;
+
+export default function SignUpForm() {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      nome: "",
+      email: "",
+      senha: "",
+      confirmacaoSenha: "",
+    },
+  });
+
+  function onSubmit(values: FormValues) {
+    console.log("Formulario valido e enviado!");
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Criar conta</CardTitle>
+          <CardDescription>Crie uma conta para continuar.</CardDescription>
+        </CardHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <CardContent className="grid gap-6">
+              <FormField
+                control={form.control}
+                name="nome"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu nome" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="senha"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Digite sua senha"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmacaoSenha"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirmar senha</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Confirme sua senha"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button>Criar conta</Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </>
+  );
+}
