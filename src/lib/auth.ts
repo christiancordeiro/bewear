4b9c7a046abc2da49c73d539be430365be974a0c
christiancordeiro <christiancordeiro96@gmail.com>
2025-08-05 21:32:23 -0300

    feat: add sign in with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,6 +8,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
     schema,
@@ -19,6 +25,9 @@ export const auth = betterAuth({
     modelName: "sessionTable",
   },
   account: {
-    modelName: "account",
+    modelName: "accountTable",
+  },
+  verification: {
+    modelName: "verificationTable",
   },
 });
