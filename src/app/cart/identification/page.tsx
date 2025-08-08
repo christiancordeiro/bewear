30ba456a17222c448f9b56b13b42b4f892d912e0
christiancordeiro <christiancordeiro96@gmail.com>
2025-08-08 18:04:26 -0300

    add cart summary component

--- src/app/cart/identification/page.tsx
+++ src/app/cart/identification/page.tsx
@@ -2,11 +2,13 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import Footer from "@/components/commom/footer";
 import Header from "@/components/commom/header";
 import { db } from "@/db";
 import { shippingAddressTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
+import CartSummary from "../components/cart-summary";
 import Addresses from "./components/addresses";
 
 export default async function IndetificationPage() {
@@ -40,14 +42,35 @@ export default async function IndetificationPage() {
     where: eq(shippingAddressTable.userId, session.user.id),
   });
 
+  const cartTotalInCents = cart.items.reduce(
+    (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
+    0,
+  );
+
   return (
     <>
-      <Header />
-      <div className="px-5">
-        <Addresses
-          shippingAddresses={shippingAddresses}
-          defaultShippingAddressId={cart.shippingAddress?.id || null}
-        />
+      <div className="space-y-12">
+        <Header />
+        <div className="space-y-4 px-5">
+          <Addresses
+            shippingAddresses={shippingAddresses}
+            defaultShippingAddressId={cart.shippingAddress?.id || null}
+          />
+
+          <CartSummary
+            subtotalInCents={cartTotalInCents}
+            totalInCents={cartTotalInCents}
+            products={cart.items.map((item) => ({
+              id: item.productVariant.id,
+              name: item.productVariant.product.name,
+              variantName: item.productVariant.name,
+              quantity: item.quantity,
+              priceInCents: item.productVariant.priceInCents,
+              imageUrl: item.productVariant.imageUrl,
+            }))}
+          />
+        </div>
+        <Footer />
       </div>
     </>
   );
